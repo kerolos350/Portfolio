589adb964c40bc6934284ce0a72417a77c7c3a31
Kerolos Emad <58334774+kerolos350@users.noreply.github.com>
2025-05-12 14:23:37 +0300

    Add loader functionality to main.js for improved user experience
    - Implemented a timeout to hide the loader after 5 seconds
    - Enabled vertical scrolling on the body once the loader is hidden

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function() {
+            document.getElementById('loader').style.display = 'none';
+            document.body.style.overflowY = 'auto';
+    }, 5000);
+});
