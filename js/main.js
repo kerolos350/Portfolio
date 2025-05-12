374c10035b46150726924febddf68e5029ab64f1
Kerolos Emad <58334774+kerolos350@users.noreply.github.com>
2025-05-12 17:29:21 +0300

    feat: enhance header animations and add hover effects for buttons and project cards

--- js/main.js
+++ js/main.js
@@ -2,5 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
     setTimeout(function() {
             document.getElementById('loader').style.display = 'none';
             document.body.style.overflowY = 'auto';
+            
+            const motion = window.matchMedia("(prefers-reduced-motion: reduce)").matches
+
+            if (motion === false) {
+                gsap.from(".header", {
+                    duration: 1,
+                    y: -100,
+                    ease: "power2.inOut"
+                });
+            }
     }, 5000);
 });
