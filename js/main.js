055a5cd96e3b4ce354ebd0144e45e6c25bd5a8ac
Kerolos Emad <58334774+kerolos350@users.noreply.github.com>
2025-06-01 07:02:53 +0300

    feat: add catch-phrase class to service and project sections, enhance animations for service cards and project labels

--- js/main.js
+++ js/main.js
@@ -7,6 +7,11 @@ document.addEventListener("DOMContentLoaded", function () {
                 skill[i].style.opacity = 0;
             }
 
+            const serviceCards = document.querySelectorAll("#services .service-card");
+            const aboutLabels = document.querySelectorAll("#about .about-label");
+            const projectLabels = document.querySelectorAll("#projects .raw h3");
+            const projects = document.querySelectorAll("#projects .projects .project");
+
             document.getElementById('loader').style.display = 'none';
             document.body.style.overflowY = 'auto';
             
@@ -48,18 +53,20 @@ document.addEventListener("DOMContentLoaded", function () {
                     delay: 0.5
                 })
 
-                gsap.from("#about .about-label", {
-                    scrollTrigger: {
-                        trigger: "#about .about-label",
-                        start: "top 70%",
-                        toggleActions: "play none none reverse"
-                    },
-                    duration: 1,
-                    x: -100,
-                    opacity: 0,
-                    ease: "power4.out",
-                    delay: 0.7
-                });
+                aboutLabels.forEach((label, index) => {
+                    gsap.from(label, {
+                        scrollTrigger: {
+                            trigger: label,
+                            start: "top 70%",
+                            toggleActions: "play none none reverse"
+                        },
+                        duration: 1,
+                        x: -100,
+                        opacity: 0,
+                        ease: "power4.out",
+                        delay: 0.5 + (index * 0.2)
+                    });
+                })
 
                 gsap.from("#about .about-content", {
                     scrollTrigger: {
@@ -88,6 +95,96 @@ document.addEventListener("DOMContentLoaded", function () {
                     duration: 1,
                     force3D: true
                 });
+
+                gsap.from("#services h2, #services .catch-phrase", {
+                    scrollTrigger: {
+                        trigger: "#services",
+                        start: "top 70%",
+                        toggleActions: "play none none reverse"
+                    },
+                    duration: 1,
+                    y: 150,
+                    opacity: 0,
+                    ease: "power4.out",
+                    delay: 0.5
+                })
+
+                serviceCards.forEach((card, index) => {
+                    gsap.from(card, {
+                        scrollTrigger: {
+                            trigger: card,
+                            start: "top 70%",
+                            toggleActions: "play none none reverse"
+                        },
+                        duration: 1,
+                        x: -150,
+                        opacity: 0,
+                        ease: "power4.out",
+                        delay: 0.5 + (index * 0.2),
+                        stagger: 0.2
+                    });
+                });
+
+                gsap.from("#projects h2, #projects .catch-phrase", {
+                    scrollTrigger: {
+                        trigger: "#projects",
+                        start: "top 70%",
+                        toggleActions: "play none none reverse"
+                    },
+                    duration: 1,
+                    y: 150,
+                    opacity: 0,
+                    ease: "power4.out",
+                    delay: 0.5
+                })
+
+                projectLabels.forEach((label, index) => {
+                    gsap.from(label, {
+                        scrollTrigger: {
+                            trigger: label,
+                            start: "top 90%",
+                            toggleActions: "play none none reverse"
+                        },
+                        duration: 1,
+                        x: -100,
+                        opacity: 0,
+                        ease: "power4.out",
+                        delay: 0.5 + (index * 0.2)
+                    });
+                });
+
+                projects.forEach((project, index) => {
+                    gsap.from(project, {
+                        scrollTrigger: {
+                            trigger: project,
+                            start: "top 90%",
+                            toggleActions: "play none none reverse"
+                        },
+                        duration: 1,
+                        x: -100,
+                        opacity: 0,
+                        ease: "power4.out",
+                        delay: 0.5 + (index * 0.2),
+                        stagger: 0.2
+                    });
+
+                    project.addEventListener("mouseenter", function() {
+                        gsap.to(project, {
+                            scale: 1.05,
+                            ease: "power2.out",
+                            duration: 0.3
+                        });
+                    })
+                    project.addEventListener("mouseleave", function() {
+                        gsap.to(project, {
+                            scale: 1,
+                            ease: "power2.out",
+                            duration: 0.3
+                        });
+                    });
+                    
+                });
+                
             }
     }, 5000);
 });
